bfdd1894a59bf16c6003e2ddb22e2ad46ae2382e
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-05-29 02:11:01 +0530

    sg

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "peering" {
   count = var.is_peering_required ? 1 : 0
-  vpc_id        = aws_vpc.main.id # requestor VPC
+  vpc_id        = aws_vpc.main.id           # requestor VPC
   peer_vpc_id   = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id
   auto_accept = var.acceptor_vpc_id == "" ? true : false
   tags = merge(
@@ -12,7 +12,7 @@ resource "aws_vpc_peering_connection" "peering" {
   )
 }
 
-# count is useful to control when resource is required
+# count is useful to control when resource is required ##
 resource "aws_route" "public_peering" {
   count = var.is_peering_required && var.acceptor_vpc_id == "" ? 1 : 0
   route_table_id            = aws_route_table.public.id
@@ -36,7 +36,7 @@ resource "aws_route" "database_peering" {
 
 resource "aws_route" "default_peering" {
   count = var.is_peering_required && var.acceptor_vpc_id == "" ? 1 : 0
-  route_table_id            = data.aws_route_table.main.id                      # default vpc route table
+  route_table_id            = data.aws_route_table.main.id # default vpc route table
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
